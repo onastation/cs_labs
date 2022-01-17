6a6cf0dccfd9e5c3d224a8337d89447d572be452
Anastasia Rybalko <imonastation@gmail.com>
2022-01-17 06:19:37 +0200

    minor fixes

--- security_lab2/main.py
+++ security_lab2/main.py
@@ -5,7 +5,7 @@ def do_xoring(line_x, line_y):
 
 def check_text(raw_lines, lines_count, actual_text, suspected_text):
     for j in range(lines_count):
-        print('New try')
+        print('-' * 50, '\nNew try')
 
         line_index = j
 
@@ -16,22 +16,22 @@ def check_text(raw_lines, lines_count, actual_text, suspected_text):
             deciphered = do_xoring(do_xoring(raw_lines[line_index], raw_lines[i]),
                                    bytearray.fromhex(''.join(hex(ord(c))[2:] for c in suspected_text)))
             deciphered_text = deciphered[:len(suspected_text)].decode("utf-8")
-            print(f'line {i} after xor: {deciphered_text}')
+            print('result text: ', deciphered_text)
             suspected_deciphered[i] = deciphered
             chars = set('#@%^*${}[]+=~/<>\\')
             if any((c in chars) for c in deciphered_text):
                 likely_wrong = True
 
         if not likely_wrong:
-            print('\nSaving results\n', '-' * 50)
+            print('\nSaving results\n')
             for line_number, line in suspected_deciphered.items():
                 actual_text[line_number] = line[:len(suspected_text)].decode('utf-8')
         else:
-            print('\nLikely wrong, results will not be saved\n', '-' * 50)
+            print('\nLikely wrong, results will not be saved\n')
 
-        print('Result:')
+        print('Actual text:')
         for line_number, line in actual_text.items():
-            print(line_number, ' | ', line)
+            print(line)
 
 
 if __name__ == '__main__':
